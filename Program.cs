1973f087d7b69e6a381cc6687a450690f3ec034c
yousefosm25 <yousefmouse1@gmail.com>
2024-02-12 00:58:34 +0200

    my file

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ConsoleApp1
+{
+  static class Program
+  {
+    static void Main()
+    {
+     Console.WriteLine("Welcome to calculate");
+     Console.WriteLine("int your number1");
+     double num1;
+     double.TryParse(Console.ReadLine(), out num1);
+     Console.WriteLine("int your number2");
+     double num2;
+     double.TryParse(Console.ReadLine(), out num2);
+     Console.WriteLine("what do you want to do");
+     Console.WriteLine("+ int 1");
+     Console.WriteLine("- int 2");
+     Console.WriteLine("* int 3");
+     Console.WriteLine("/ int 4");
+     Console.WriteLine("% int 5");
+     int trynum;
+     int.TryParse(Console.ReadLine(), out trynum);
+     if(trynum == 1)
+     {
+      Console.WriteLine(num1+num2);     
+     }
+     else if(trynum == 2)
+     {
+      Console.WriteLine(num1-num2);
+     }
+     else if(trynum == 3)
+     {
+      Console.WriteLine(num1*num2);
+     }
+     else if(trynum == 4)
+     {
+      Console.WriteLine(num1/num2);
+     }
+     else if(trynum == 5)
+     {
+      Console.WriteLine(num1%num2);
+     }
+     else
+     {
+        Console.WriteLine("invalid input");
+     }   
+     
+      
+    }      
+  }
+}
